c25b6ce7b8e8072032b1d6698481bd04d1597ed7
peterdinis <petter1@pobox.sk>
2025-06-21 20:54:33 +0200

    feat: Basic landing page DONE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
 <script>
 	import HeroSection from "$lib/components/home/HeroSection.svelte";
 	import LiveScores from "$lib/components/home/LiveScores.svelte";
+	import NewsSection from "$lib/components/home/NewsSection.svelte";
+	import VideoSectionNews from "$lib/components/home/VideoSectionNews.svelte";
 	import Footer from "$lib/components/shared/Footer.svelte";
 	import Navigation from "$lib/components/shared/Navigation.svelte";
+	import ScrollToTop from "$lib/components/shared/ScrollToTop.svelte";
 
 </script>
 <div class="min-h-screen bg-white">
     <Navigation />
     <LiveScores />
     <HeroSection />
+    <NewsSection />
+    <VideoSectionNews />
+    <ScrollToTop />
     <Footer />
 </div>
---
+++ src/utils/fadeFly.ts
@@ -0,0 +1,28 @@
+import { cubicOut } from 'svelte/easing';
+import type { TransitionConfig } from 'svelte/transition';
+
+export function fadeFly(
+  node: Element,
+  {
+    delay = 0,
+    duration = 300,
+    easing = cubicOut,
+    x = 0,
+    y = 0,
+    opacity = 0
+  } = {}
+): TransitionConfig {
+  const style = getComputedStyle(node);
+  const target_opacity = +style.opacity;
+
+  return {
+    delay,
+    duration,
+    easing,
+    css: (t, u) =>
+      `
+        transform: translate(${x * u}px, ${y * u}px);
+        opacity: ${t * target_opacity + (1 - t) * opacity};
+      `
+  };
+}
