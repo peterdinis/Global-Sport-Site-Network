49d7027cd4750d4a9a5c76de3070e1e06f67efea
peterdinis <petter1@pobox.sk>
2025-06-21 17:52:16 +0200

    infra: Working on homepage WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+	import LiveScores from "$lib/components/home/LiveScores.svelte";
 	import Navigation from "$lib/components/shared/Navigation.svelte";
 
 </script>
 <div class="min-h-screen bg-white">
     <Navigation />
+    <LiveScores />
 </div>
